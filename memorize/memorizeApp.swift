f4e2b8940665e3120973a56d77e2353044741766
Luis Filipe Alves de Oliveira <lfoliveira.dev@gmail.com>
2021-04-17 17:04:15 -0300

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Luis Filipe Alves de Oliveira on 17/04/21.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
